ba892772f6c4cf387e62b57a29a3de1c33ecece4
CristinaNilvan <52707648+CristinaNilvan@users.noreply.github.com>
2021-04-11 23:12:02 +0300

    Update binary-search-tree

--- Trees/binary-search-tree/src/BinarySearchTree.java
+++ Trees/binary-search-tree/src/BinarySearchTree.java
@@ -50,13 +50,115 @@ public class BinarySearchTree
         return searchNode(node.right, key);
     }
 
-    public void inOrderPrinting(Node node)
+    public Node findMinimum(Node root)
     {
-        if(node != null)
+        if (root == null)
+            return null;
+
+        Node currentNode = root;
+
+        while (currentNode.left != null)
+            currentNode = currentNode.left;
+
+        return currentNode;
+    }
+
+    public Node findMaximum(Node root)
+    {
+        if (root == null)
+            return null;
+
+        Node currentNode = root;
+
+        while (currentNode.right != null)
+            currentNode = currentNode.right;
+
+        return currentNode;
+    }
+
+    public Node findSuccessor(Node root, Node node)
+    {
+        if (node == null || root == null)
+            return null;
+
+        if (node.right != null)
+            return findMinimum(node.right);
+
+        Node currentNode = root;
+        Node successor = null;
+
+        while (currentNode != null && currentNode.key != node.key)
         {
-            inOrderPrinting(node.left);
-            System.out.print(node.key + " ");
-            inOrderPrinting(node.right);
+            if (node.key < currentNode.key)
+            {
+                successor = currentNode;
+                currentNode = currentNode.left;
+            }
+            else
+                currentNode = currentNode.right;
+        }
+
+        if (currentNode == null)
+            return null;
+
+        return successor;
+    }
+
+    public Node findPredecessor(Node root, Node node)
+    {
+        if (node == null || root == null)
+            return null;
+
+        if (node.left != null)
+            return findMaximum(node.left);
+
+        Node currentNode = root;
+        Node predecessor = null;
+
+        while (currentNode != null && currentNode.key != node.key)
+        {
+            if (node.key < currentNode.key)
+                currentNode = currentNode.left;
+            else
+            {
+                predecessor = currentNode;
+                currentNode = currentNode.right;
+            }
+        }
+
+        if (currentNode == null)
+            return null;
+
+        return predecessor;
+    }
+
+    public void inOrderPrinting(Node root)
+    {
+        if(root != null)
+        {
+            inOrderPrinting(root.left);
+            System.out.print(root.key + " ");
+            inOrderPrinting(root.right);
+        }
+    }
+
+    public void preOrderPrinting(Node root)
+    {
+        if (root != null)
+        {
+            System.out.print(root.key + " ");
+            preOrderPrinting(root.left);
+            preOrderPrinting(root.right);
+        }
+    }
+
+    public void postOrderPrinting(Node root)
+    {
+        if (root != null)
+        {
+            postOrderPrinting(root.left);
+            postOrderPrinting(root.right);
+            System.out.print(root.key + " ");
         }
     }
 
@@ -86,6 +188,44 @@ public class BinarySearchTree
             System.out.println("The node has not been found.");
     }
 
+    public static void testsSuccessor(BinarySearchTree myTree)
+    {
+        int key = 15;
+        Node node = myTree.searchNode(myTree.root, key);
+
+        Node successor = myTree.findSuccessor(myTree.root, node);
+
+        if (successor != null)
+            System.out.println("The node with key = " + node.key + " has " + successor.key + " as successor.");
+        else
+            System.out.println("The node doesn't have a successor");
+    }
+
+    public static void testsPredecessor(BinarySearchTree myTree)
+    {
+        int key = 15;
+        Node node = myTree.searchNode(myTree.root, key);
+
+        Node predecessor = myTree.findPredecessor(myTree.root, node);
+
+        if (predecessor != null)
+            System.out.println("The node with key = " + node.key + " has " + predecessor.key + " as predecessor.");
+        else
+            System.out.println("The node doesn't have a predecessor");
+    }
+
+    public static void testsPrinting(BinarySearchTree myTree)
+    {
+        myTree.inOrderPrinting(myTree.root);
+        System.out.println();
+
+        myTree.preOrderPrinting(myTree.root);
+        System.out.println();
+
+        myTree.postOrderPrinting(myTree.root);
+        System.out.println();
+    }
+
     public static void main (String[] args)
     {
         BinarySearchTree myTree = new BinarySearchTree();
@@ -93,5 +233,11 @@ public class BinarySearchTree
         testsInsertion(myTree);
 
         testsSearch(myTree);
+
+        testsSuccessor(myTree);
+
+        testsPredecessor(myTree);
+
+        testsPrinting(myTree);
     }
 }
